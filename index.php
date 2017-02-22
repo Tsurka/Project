990c3eac4f2219c9bcf7c77360d380dc7d8f0366
zehov <zehov@abv.bg>
2017-02-22 22:29:50 +0200

    Project 22_02

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once('includes/header.php');
 <div class="container text-center" >
 	<h2>БЕСЕНИЦА</h2>
 	<p>Игра за губене на време и още нещо.... </p>            
-	<img src="img/foto1.png" class="img-rounded" alt="foto1" width="500" height="350"> 
+	<img src="img/foto1.png" class="img-rounded" alt="foto1" width="350" height="250"> 
 	<div  id='hello' class="col-lg-6 col-lg-offset-3 text-center"> 
  		<label for='login'> Моля попълнете  за да продължите : </label> 
 		<?php 
@@ -14,7 +14,7 @@ include_once('includes/header.php');
 		echo "<p>"; 
 		submit('LOG ON','btn btn-info'); 
 		echo "</p></form>"; 
-		echo '<a class="btn btn-default" href="#" role="button">Ако нямате ригистрация го направете <span> тук </span></a>';
+		echo '<a class="btn btn-default" href="registration.php" role="button">Ако нямате ригистрация го направете <span> тук </span></a>';
 		?> 
     </div> 
 </div>
